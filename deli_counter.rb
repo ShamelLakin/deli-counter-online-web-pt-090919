561811d9d537e3f416a5075896b0e72348044d9d
Shamel Lakin <christlife4u@gmail.com>
2019-09-16 17:43:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(katz_deli)
+  katz_deli = []
+  if katz_deli == 0 
+    puts "The line is currently empty."
+end
+
+
+
+def take_a_number(Katz_deli, "Ada")
+  Katz_deli = ["Logan", "Avi", "Spencer"]
+  Katz_deli.each_with_index do |Katz_deli, index|
+    puts "Welcome, #{Katz_deli}. You are number #{index+1} in line"
+end
+
